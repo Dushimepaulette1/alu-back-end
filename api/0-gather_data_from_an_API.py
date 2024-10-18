0de9864705dac8e4b88959b01b66a0a35d8f00bc
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-10-18 12:30:15 +0000

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """
 Uses https://jsonplaceholder.typicode.com along with an employee ID to
-return information about the employee's todo list progress
+return information about the employee's todo list progress.
 """
 
 import requests
 from sys import argv
 
 if __name__ == '__main__':
-    userId = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
-    completed_tasks = []
-    for task in todo:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(completed_tasks), len(todo)))
-    print("\n".join("\t {}".format(task) for task in completed_tasks))
+    user_id = int(argv[1])  # Ensure the user ID is treated as an integer
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id)).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)).json()
+    
+    # Get the employee name correctly
+    employee_name = user.get('name')
+    completed_tasks = [task.get('title') for task in todo if task.get('completed')]
+
+    # Output the result in the specified format
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, len(completed_tasks), len(todo)))
+    for task in completed_tasks:
+        print("\t {}".format(task))
+
