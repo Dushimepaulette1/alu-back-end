392376678dcb1f4f4cb95855b4b2724700cd025f
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-10-18 12:46:40 +0000

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,18 +8,15 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    user_id = int(argv[1])  # Ensure the user ID is treated as an integer
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
-    
+    user = int(argv[1])  # Ensure the user ID is treated as an integer
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user)
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(user)
     user = requests.get(user_url).json()
     todo = requests.get(todo_url).json()
-    
     employee_name = user.get('name')
     completed_tasks = [task.get('title') for task in todo if task.get('completed')]
 
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, len(completed_tasks), len(todo)))
-    
     for task in completed_tasks:
         print("\t {}".format(task))
