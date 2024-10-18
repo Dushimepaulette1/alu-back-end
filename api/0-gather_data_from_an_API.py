472978ce4befa05c92a962b60eed24ab821de11c
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-10-18 13:06:43 +0000

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,15 +8,23 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    u = int(argv[1])  # Ensure the user ID is treated as an integer
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(u)
-    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".fomat(u)
-    u = requests.get(user_url).json()
-    todo = requests.get(todo_url).json()
-    employee_name = u.get('name')
-    completed_tasks = [task.get('title') for task in todo if task.get('comp')]
+    user_id = argv[1]
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(user_id),
+        verify=False
+    ).json()
+    todo = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id),
+        verify=False
+    ).json()
+
+    completed_tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            completed_tasks.append(task.get('title'))
 
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(completed_tasks), len(todo)))
-    for task in completed_tasks:
-        print("\t {}".format(task))
+        user.get('name'), len(completed_tasks), len(todo)
+    ))
+
+    print("\n".join("\t {}".format(task) for task in completed_tasks))
